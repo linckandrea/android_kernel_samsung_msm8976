0a42a96ffa92377950d5648ae15fb8ee43191680
linckandrea <linckandrea01@gmail.com>
2025-02-20 05:55:20 +0100

    Revert "sdcardfs: kang from msm_8994"
    This reverts commit 2fbce786d04887d75780e2ef37926c98e761c69e.

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -123,12 +123,6 @@ out:
 	return err;
 }
 
-/* 1 = delete, 0 = cache */
-static int sdcardfs_d_delete(const struct dentry *d)
-{
-	return SDCARDFS_SB(d->d_sb)->options.nocache ? 1 : 0;
-}
-
 static void sdcardfs_d_release(struct dentry *dentry)
 {
 	if (!dentry || !dentry->d_fsdata)
@@ -187,9 +181,14 @@ static void sdcardfs_canonical_path(const struct path *path,
 	sdcardfs_get_real_lower(path->dentry, actual_path);
 }
 
+static int sdcardfs_d_delete(const struct dentry * dentry)
+{
+	return dentry->d_inode && !S_ISDIR(dentry->d_inode->i_mode);
+}
+
 const struct dentry_operations sdcardfs_ci_dops = {
-	.d_revalidate	= sdcardfs_d_revalidate,
 	.d_delete	= sdcardfs_d_delete,
+	.d_revalidate	= sdcardfs_d_revalidate,
 	.d_release	= sdcardfs_d_release,
 	.d_hash	= sdcardfs_hash_ci,
 	.d_compare	= sdcardfs_cmp_ci,
